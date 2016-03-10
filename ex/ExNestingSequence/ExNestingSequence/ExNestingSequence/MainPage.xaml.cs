52ee0d60a8203e7d7ca4f25cc70cdf992d97a33b
Daniel Inversini <inved1@bfh.ch>
2016-03-10 09:30:17 +0100

    initial commit

---
+++ ex/ExNestingSequence/ExNestingSequence/ExNestingSequence/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ExNestingSequence
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void b1_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            b1.Opacity = (b1.Opacity < 0.1) ? 1.0 : b1.Opacity - 0.25;
+            t1.Text = String.Format("Text before / Opacity = {0}", b1.Opacity);
+        }
+
+        private void b2_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            b2.Opacity = (b2.Opacity < 0.1) ? 1.0 : b2.Opacity - 0.25;
+            t2.Text = String.Format("Text inside / Opacity = {0}", b2.Opacity);
+        }
+
+        private void b3_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            b3.Opacity = (b3.Opacity < 0.1) ? 1.0 : b3.Opacity - 0.25;
+            t3.Text = String.Format("Text after / Opacity = {0}", b3.Opacity);
+        }
+    }
+}
---
+++ ex/ExPropertiesAndEvents/ExPropertiesAndEvents/ExPropertiesAndEvents/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ExPropertiesAndEvents
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+
+            int col = (int)Grid.GetColumn(element);
+            int row = (int)Grid.GetRow(element);
+
+            if ((col >= 2) || (row >= 2))
+                MainGrid.Children.Remove(element);
+            else if (col > row)
+                Grid.SetRow(element, row + 1);
+            else
+                Grid.SetColumn(element, col + 1);
+        }
+
+        private void MainGrid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var btn = new Button();
+            btn.Content = (new Random()).Next();
+            btn.Tapped += Button_Tapped;
+            Grid.SetColumn(btn, 0);
+            Grid.SetRow(btn, 0);
+            MainGrid.Children.Add(btn);
+        }
+
+        bool isYellow = true;
+        Brush bg = null;
+
+        private void Border_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Border border = sender as Border;
+
+            if (isYellow)
+            {
+                bg = border.Background;
+                border.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+                e.Handled = true;
+            }
+            else
+            {
+                border.Background = bg;
+            }
+
+            isYellow = !isYellow;
+        }
+    }
+}
