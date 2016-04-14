c81b8af14d8b509795fbae2b2953e5e208b13b29
Daniel Inversini <inved1@bfh.ch>
2016-04-14 08:33:59 +0200

    some updates

---
+++ ex/ExAgenda10/ExAgenda10/AgendaItem.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace ExAgenda10
+{
+    public sealed partial class AgendaItem : UserControl
+    {
+        public AgendaItem()
+        {
+            this.InitializeComponent();
+        }
+
+        public new Brush Background
+        {
+            get { return this.GridBG.Background; }
+            set { this.GridBG.Background = value; }
+        }
+
+        public new Brush Foreground
+        {
+            get { return this.TextText.Foreground; }
+            set
+            {
+                this.TextText.Foreground = value;
+                this.TextTime.Foreground = value;
+                this.SymbolAttachment.Foreground = value;
+                this.SymbolImportant.Foreground = value;
+            }
+        }
+
+        public string Text
+        {
+            get { return this.TextText.Text; }
+            set { this.TextText.Text = value; }
+        }
+
+        int day = 0;
+        public int Day
+        {
+            get { return day; }
+            set
+            {
+                day = (value < 0) ? 0 : value % 7;
+                Grid.SetColumn(this, day + 1);
+            }
+        }
+
+        public string Time
+        {
+            get { return this.TextTime.Text; }
+            set
+            {
+                this.TextTime.Text = value;
+                var v = value.Split(':', '-');
+                int s = Int32.Parse(v[0]) * 2 + Int32.Parse(v[1]) / 30;
+                int e = Int32.Parse(v[2]) * 2 + Int32.Parse(v[3]) / 30;
+                Grid.SetRow(this, s + 1);
+                Grid.SetRowSpan(this, e - s);
+            }
+        }
+
+        static SolidColorBrush PrioBrush1 = new SolidColorBrush(Color.FromArgb(255, 0, 240, 240));
+        static SolidColorBrush PrioBrush2 = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+
+        public int Priority
+        {
+            get
+            {
+                if (this.BorderPrio.Visibility == Visibility.Visible)
+                {
+                    if (this.BorderPrio.Background == PrioBrush1)
+                        return 1;
+                    else if (this.BorderPrio.Background == PrioBrush2)
+                        return 2;
+                }
+                return 0;
+            }
+            set
+            {
+                if (value == 1)
+                    this.BorderPrio.Background = PrioBrush1;
+                else if (value == 2)
+                    this.BorderPrio.Background = PrioBrush2;
+
+                if ((value == 1) || (value == 2))
+                    this.BorderPrio.Visibility = Visibility.Visible;
+                else
+                    this.BorderPrio.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        public bool IsImportant
+        {
+            get { return this.SymbolImportant.Visibility == Visibility.Visible; }
+            set { this.SymbolImportant.Visibility = (value) ? Visibility.Visible : Visibility.Collapsed; }
+        }
+
+        public bool HasAttachments
+        {
+            get { return this.SymbolAttachment.Visibility == Visibility.Visible; }
+            set { this.SymbolAttachment.Visibility = (value) ? Visibility.Visible : Visibility.Collapsed; }
+        }
+    }
+}
---
+++ ex/ExAgenda10Databound/ExAgenda10Databound/AgendaItem.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ExAgenda10Databound
+{
+    public enum Priority { None, Normal, High };
+    public enum Category { None, Business, Private, Education };
+
+    public class AgendaItem
+    {
+        public string Text { get; set; }
+        public DateTime StartTime { get; set; }
+        public int Duration { get; set; }
+        public string Time
+        {
+            get
+            {
+                return String.Format("{0} - {1}",
+                    StartTime.ToString("HH:mm"),
+                    StartTime.AddMinutes(Duration).ToString("HH:mm"));
+            }
+        }
+        public Category Category { get; set; }
+        public Priority Priority { get; set; }
+        public bool IsImportant { get; set; }
+        public bool HasAttachments { get; set; }
+    }
+
+    public class Week : ObservableCollection<AgendaItem>
+    {
+        public Week()
+        {
+            Add(new AgendaItem
+            {
+                Text = "Dept-Meeting",
+                StartTime = new DateTime(2015, 4, 13, 8, 0, 0),
+                Duration = 120,
+                Category = Category.Business,
+                Priority = Priority.Normal,
+                IsImportant = false,
+                HasAttachments = false
+            });
+            Add(new AgendaItem
+            {
+                Text = "Lunch with Donald",
+                StartTime = new DateTime(2015, 4, 14, 12, 0, 0),
+                Duration = 60,
+                Category = Category.None,
+                Priority = Priority.None,
+                IsImportant = false,
+                HasAttachments = false
+            });
+            Add(new AgendaItem
+            {
+                Text = "Tennis",
+                StartTime = new DateTime(2015, 4, 14, 19, 0, 0),
+                Duration = 120,
+                Category = Category.Private,
+                Priority = Priority.None,
+                IsImportant = false,
+                HasAttachments = false
+            });
+            Add(new AgendaItem
+            {
+                Text = "Prepare Financial Report",
+                StartTime = new DateTime(2015, 4, 15, 9, 0, 0),
+                Duration = 180,
+                Category = Category.Business,
+                Priority = Priority.High,
+                IsImportant = true,
+                HasAttachments = true
+            });
+            Add(new AgendaItem
+            {
+                Text = "Financial Meeting, N.216",
+                StartTime = new DateTime(2015, 4, 15, 13, 0, 0),
+                Duration = 120,
+                Category = Category.Business,
+                Priority = Priority.High,
+                IsImportant = true,
+                HasAttachments = false
+            });
+            Add(new AgendaItem
+            {
+                Text = "Workshop 'New Marketing Strategy', New York",
+                StartTime = new DateTime(2015, 4, 16, 9, 0, 0),
+                Duration = 600,
+                Category = Category.Education,
+                Priority = Priority.None,
+                IsImportant = true,
+                HasAttachments = true
+            });
+            Add(new AgendaItem
+            {
+                Text = "Lunch with Daisy",
+                StartTime = new DateTime(2015, 4, 17, 12, 30, 0),
+                Duration = 90,
+                Category = Category.None,
+                Priority = Priority.None,
+                IsImportant = false,
+                HasAttachments = false
+            });
+            Add(new AgendaItem
+            {
+                Text = "Check Time",
+                StartTime = new DateTime(2015, 4, 18, 13, 0, 0),
+                Duration = 30,
+                Category = Category.Business,
+                Priority = Priority.High,
+                IsImportant = true,
+                HasAttachments = false
+            });
+            Add(new AgendaItem
+            {
+                Text = "Tennis",
+                StartTime = new DateTime(2015, 4, 17, 19, 30, 0),
+                Duration = 120,
+                Category = Category.Private,
+                Priority = Priority.None,
+                IsImportant = false,
+                HasAttachments = false
+            });
+        }
+
+    }
+}
